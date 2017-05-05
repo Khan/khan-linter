d8e0fb484693a9ae48568ff653509da734240b3f
Charles Marsh <charlie@khanacademy.org>
2017-05-05 08:37:17 -0400

    Set working directory in python_version_compat_test.py
    Summary:
    These tests were passing when run from within `khan-linter`, but failing when run via `webapp`'s
    `tools/runtests.py`. The issue seems to be that when run from `webapp`, the current working
    directory in the `subprocess` calls was `~/khan/webapp`, and so `runlint.py` couldn't be found.
    Setting the working directory explicitly to match the test path resolves the issue.
    
    Test Plan:
    - From `khan-linter`, run: `python -m unittest python_version_compat_test`.
        - Verify that both tests pass without error.
    - From: `~/khan/webapp`, run: `./tools/runtests.py third_party/khan-linter-src/python_version_compat_test.py`.
        - Verify that both tests pass without error.
    - Introduce some non-Python 3 compatible into `runlint.py` (e.g., `print 'broken'`).
    - Re-run the tests from the two locations listed above.
        - Verify that the Python 3 test fails in both cases, but the Python 2 test passes.
    
    Reviewers: colin
    
    Reviewed By: colin
    
    Subscribers: csilvers, kevinb
    
    Differential Revision: https://phabricator.khanacademy.org/D35546

--- python_version_compat_test.py
+++ python_version_compat_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import subprocess
 import unittest
 
@@ -10,7 +11,14 @@ class TestPy2Py3Compat(unittest.TestCase):
     Test that we can at least import runlint.py under both.
     """
     def test_python2_compat(self):
-        subprocess.check_call(['python2', '-c',  'import runlint'])
+        # If we're running this test from an external directory (e.g., from
+        # webapp), take care to set the working directory for the subprocess
+        # call. Note that if we're running this test from within
+        # khan-linter-src, we need the cwd to be None, rather than the empty
+        # string.
+        cwd = os.path.dirname(__file__) or None
+        subprocess.check_call(['python2', '-c',  'import runlint'], cwd=cwd)
 
     def test_python3_compat(self):
-        subprocess.check_call(['python3', '-c',  'import runlint'])
+        cwd = os.path.dirname(__file__) or None
+        subprocess.check_call(['python3', '-c',  'import runlint'], cwd=cwd)
