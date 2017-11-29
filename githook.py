a07db67005e456cb551a6fd672fd572e744623cb
Matt Dunn-Rankin <mattdunnrankin@khanacademy.org>
2017-11-29 12:49:13 -0800

    Add a pre-push lint hook to khan-linter (not yet installed in webapp)
    Summary:
    As we integrate Prettier auto-JS-formatting into webapp (and into our linter), we need to change our general linting workflow. Currently, we use a git hook to reject commits whose changes fail linting - but, once Prettier is installed, changes to JS are generally //expected// to fail linting.
    
    So, to improve the linting UX, we plan to stop running the linter at commit time. Instead, we'll continue to lint at `arc diff` time (which has a nice autofixer UI, so Prettier changes will be easy to accept!), and we'll add a new pre-push hook to continue to make it difficult to accidentally push unlinted changes via other workflows (e.g. post-push review).
    
    In this change, we implement the pre-push hook logic in `khan-linter`. It looks for commits that will be pushed to a remote repo for the first time, and lints the files added/modified in each such commit. It is invoked via `~/khan/devtools/khan-linter/githook.py --hook=pre-push remote_name remote_location`.
    
    This change does not affect webapp, or the ka-clone tools that install git hooks in webapp. We also take care not to break existing webapp git hooks, by leaving the `commit-msg` hook invocation syntax unchanged.
    
    In a subsequent change, we'll update `ka-clone` to install a corresponding pre-push git hook in webapp, which will invoke this `khan-linter` script. After that, we'll update the `commit-msg` handler in `khan-linter` to no longer lint files, and only lint the commit message.
    
    Test Plan:
    Add a temporary line to the pre-push handler in `githook.py`, to print the set of linted files. Then, run `test_githook_pre_push.sh`, and manually confirm that the actual set of linted files always matches the expected set.
    
    Additionally, I installed this as a pre-push git hook in `webapp` as well, and did some very basic testing to confirm that integration works as expected.
    
    Reviewers: csilvers, john, kevinb
    
    Reviewed By: csilvers, john
    
    Subscribers: jared
    
    Differential Revision: https://phabricator.khanacademy.org/D41012

--- githook.py
+++ githook.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python
+"""Hooks for git that perform linting.
 
-"""Commit hook for git that does lint testing.
+This file contains two git hooks:
 
-It runs the lint checker (runlint.py) on all the open files in
-the current commit.
+1. A commit-msg hook, to lint the commit message. If lint fails, the commit
+   is aborted, and the message is saved in a temporary file.
+2. A pre-push hook, to lint files that will remotely change as a result of the
+   push. If lint fails, the push is aborted.
 
-To install (for git >= 1.7.1), run the following:
-   % git config --global init.templatedir '~/.git_template'
-and then create a symlink from
-   ~/.git_template/hooks/commit-msg
-to this file.
+These hooks are automatically installed in linted KA repositories, by the
+`ka-clone` tool.
+
+This script accepts an optional argument "--hook=<hook>", which specifies which
+hook to run (commit-msg or pre-push). If omitted, defaults to "commit-msg", to
+preserve legacy behavior (from back when that was the only hook this script
+contained).
+
+The remaining positional arguments are assumed to be provided by git, and are
+forwarded to the hook's handler function as the argument list. (For example,
+the commit-msg git hook has a single positional argument: the path to the
+commit message file.) Note, too, that some git hooks provide additional input
+on STDIN.
+
+The user can choose to skip this hook, by running `git push --no-verify` (in
+which case, git won't call this script at all), or specifying FORCE_COMMIT=1
+in the environment (in which case, this script will be a no-op).
+
+TODO(mdr): In addition to linting the commit message, the commit-msg hook
+    currently lints the commit's changed files, and rejects the commit if lint
+    fails - but this use case is now effectively covered by the pre-push hook.
+    Stop linting file changes on commit, and remove the corresponding dead
+    code in hook_lib.py. (JSBUILD-160)
+
+NOTE(mdr): If you're here to remove a hook, be aware that existing ka-clone'd
+    repositories on developers' machines might still attempt to call the hook.
+    Instead of deleting the hook outright and triggering a crash, consider
+    replacing the hook's handler with a message explaining how to upgrade.
 """
 
+import argparse
 import os
 import subprocess
 import sys
@@ -28,7 +55,7 @@ def _normalized_commit_message(text):
     return ''.join(lines).strip()
 
 
-def main(commit_message_file):
+def commit_msg_hook(commit_message_file):
     """Run a git pre-commit lint-check."""
     # If we're a merge, don't try to do a lint-check.
     git_root = subprocess.check_output(
@@ -119,7 +146,109 @@ def main(commit_message_file):
                                     recommendation=recommendation)
 
 
+def pre_push_hook(_unused_arg_remote_name, _unused_arg_remote_location):
+    """Run a git pre-push lint-check.
+
+    The pre-push hook has a test script: test_githook_pre_push.sh. If you're
+    making significant changes to this function, consider running the test!
+    """
+    for line in sys.stdin:
+        # For each branch we're trying to push, the git hook will tell us the
+        # local branch name, local sha, remote branch name, and remote sha.
+        # For our purposes, we only care about the local sha.
+        (_, local_sha, _, _) = line.split()
+
+        # To find files that have been changed locally, we'll use `git log` to
+        # find commits that are present in the branch state we intend to push,
+        # but aren't present on any remote-tracking branch. We'll then format
+        # the list of files changed in each such commit.
+        #
+        # This filtering is based on the assumption that any change already on
+        # the remote server, and therefore in a remote-tracking branch, has
+        # been linted by a previous `git push` hook.
+        #
+        # This filtering is just an optimization, and it's okay if we re-lint
+        # a few extra files... but we should try not to lint *too* many extra
+        # files, or else the process will be annoyingly slow.
+        #
+        # NOTE(mdr): This filtering won't recognize changes that are already on
+        #     on remote, but were *copied* to the local branch, for example by
+        #     `git cherry-pick`. The `--cherry-pick` filtering option for
+        #     `git log` claims to be able to detect this, but only works for
+        #     symmetric difference filtering, which isn't what we're using.
+        #     Thankfully, this isn't a big deal in standard workflows for
+        #     merging or rebasing master's work into a feature branch. Merging
+        #     will use the original commits from both branches. Rebasing is
+        #     liable to copy the feature branch's commits, but those are likely
+        #     to have a much smaller surface area, so re-linting them shouldn't
+        #     take very long.
+        #
+        # NOTE(mdr): A simpler filtering strategy might be to just compare the
+        #     local and remote state of the current branch. However, this would
+        #     lead to unnecessarily slow linting for merge commits: e.g., you
+        #     merge origin/master into local branch foobar, then push to
+        #     origin/foobar. This push might contain many commits from
+        #     origin/master which aren't yet in origin/foobar, but that *have*
+        #     been linted already.
+        files_to_lint = subprocess.check_output([
+            'git', 'log',
+
+            # We "format" each commit by hiding its commit details, and listing
+            # the name of each added/modified/removed file, separated by NUL.
+            '--pretty=format:', '--name-only', '--diff-filter=AMR', '-z',
+
+            # Include commits that are reachable from the local state we intend
+            # to push.
+            local_sha,
+
+            # Do not include commits that are reachable from any
+            # remote-tracking branch's state. (The `--remotes` flag is
+            # equivalent to manually listing `origin/master`, `origin/foobar`,
+            # etc. The `--not` flag negates all subsequent references.)
+            '--not', '--remotes',
+        ]).decode('utf-8')
+
+        # Parse files to lint: split at NUL characters, remove blank entries,
+        # and remove duplicates.
+        files_to_lint = list({f for f in files_to_lint.split('\0') if f})
+
+        # Lint the files, if any. If there are any errors, print a helpful
+        # message, and return a nonzero status code to abort the push.
+        if files_to_lint:
+            num_errors = hook_lib.lint_files(files_to_lint)
+            if num_errors > 0:
+                six.print_(
+                    '\n--- %s lint errors. Push aborted. ---' % num_errors,
+                    file=sys.stderr)
+                six.print_(
+                    'Running `arc lint` may help to autofix the errors.',
+                    file=sys.stderr)
+                return 1
+
+        # Yay, everything went okay! Return a zero status code, in order to
+        # allow the push.
+        six.print_('khan-linter: all lint checks passed', file=sys.stderr)
+        return 0
+
+
 if __name__ == '__main__':
     suppress_lint = os.getenv('FORCE_COMMIT', '')
-    if suppress_lint.lower() not in ('1', 'true'):
-        sys.exit(main(sys.argv[1]))
+    if suppress_lint.lower() in ('1', 'true'):
+        sys.exit(0)
+
+    parser = argparse.ArgumentParser(description='Perform lint git hooks.')
+    parser.add_argument('--hook', type=str, choices=['commit-msg', 'pre-push'],
+                        default='commit-msg', help='which hook to perform')
+    parser.add_argument('hook_args', nargs='*',
+                        help='The arguments provided by the git hook.')
+
+    args = parser.parse_args()
+    if args.hook == 'commit-msg':
+        status_code = commit_msg_hook(*args.hook_args)
+    elif args.hook == 'pre-push':
+        status_code = pre_push_hook(*args.hook_args)
+    else:
+        raise AssertionError("unrecognized hook name - should've been caught "
+                             "by argparse?")
+
+    sys.exit(status_code)
---
+++ test_githook_pre_push.sh
@@ -0,0 +1,162 @@
+#!/usr/bin/env bash
+set -e
+
+# This is a script to test the pre-push lint hook in githook.py. It creates a
+# test repository, installs the lint git hook, then runs a series of tests to
+# confirm that the right files are linted on push, and that lint failure aborts
+# the push. If the test passed, it will output "Test passed!".
+#
+# Unfortuantely, this script doesn't *automatically* assert that the correct
+# files are linted. To verify this, add a temporary line of code to githook.py
+# that prints the set of linted files, then manually check the output against
+# the "Confirm:" lines outputted by this script.
+#
+# TODO(mdr): It'd be nice if this script *automatically* asserted that the
+#     right files are linted. I'm not sure it's worth building the necessary
+#     infra right now, though, since this probably won't be a high-traffic area
+#     of the KA codebase. But, if that changes, let's upgrade this script!
+
+remote_repo_path=$(mktemp -d -t lint-hook-test-remote)
+local_repo_path=$(mktemp -d -t lint-hook-test-local)
+function finish {
+  rm -rf $remote_repo_path
+  rm -rf $local_repo_path
+}
+trap finish EXIT
+
+# Create a local git repository to serve as the "remote" repository.
+echo '>> Initialize "remote" repo.'
+git init --bare $remote_repo_path
+echo ''
+
+# Create a local git repository to serve as the "local" repository.
+# Initialize it, set up the remote, and add the khan-linter git hook.
+echo '>> Initialize "local" repo.'
+cd $local_repo_path
+git init
+git remote add origin $remote_repo_path
+git commit --allow-empty -m 'initial commit'
+git push -u origin master
+echo '~/khan/devtools/khan-linter/githook.py --hook=pre-push "$@"' > .git/hooks/pre-push
+chmod +x .git/hooks/pre-push
+echo ''
+
+echo ''
+echo '>> Push. (Changes: <none>.)'
+echo '>> Confirm: No files are linted, push succeeds.'
+git push
+echo ''
+
+echo '>> Commit. (Changes: Add A and B.)'
+touch A.js B.js
+git add A.js B.js
+git commit -m 'Add A and B.'
+git tag add-A-and-B
+echo ''
+
+echo '>> Push. (Changes: Add A and B.)'
+echo '>> Confirm: A and B are linted, push succeeds.'
+git push
+echo ''
+
+echo '>> Commit. (Changes: Modify A to fail linting.)'
+echo 'foo' > A.js
+git add A.js
+git commit -m 'Modify A to fail linting.'
+echo ''
+
+echo '>> Push. (Changes: Modify A to fail linting.)'
+echo '>> Confirm: A is linted, push fails.'
+! git push
+echo ''
+
+echo '>> Revert changes to A.'
+git reset --hard add-A-and-B
+echo ''
+
+echo '>> Commit. (Changes: Add C, which fails linting.)'
+echo 'foo' > C.js
+git add C.js
+git commit -m 'Add C, which fails linting.'
+echo ''
+
+echo '>> Push. (Changes: Add C, which fails linting.)'
+echo '>> Confirm: C is linted, push fails.'
+! git push
+echo ''
+
+echo '>> Revert creation of C.'
+git reset --hard add-A-and-B
+echo ''
+
+echo '>> Create feature branch.'
+git checkout -b feature-branch
+echo ''
+
+echo '>> Commit. (Changes: Add D on feature branch.)'
+touch D.js
+git add D.js
+git commit -m 'Add D on feature branch.'
+echo ''
+
+echo '>> Checkout master.'
+git checkout master
+echo ''
+
+echo '>> Commit. (Changes: Add E.)'
+touch E.js
+git add E.js
+git commit -m 'Add E.'
+echo ''
+
+echo '>> Push. (Changes: Add E.)'
+echo '>> Confirm: E is linted, push succeeds.'
+git push
+echo ''
+
+echo '>> Checkout feature branch.'
+git checkout feature-branch
+echo ''
+
+echo '>> Merge master.'
+git merge master --no-edit
+echo ''
+
+echo '>> Push feature branch. (Changes: Add E on feature branch, merge D from master.)'
+echo '>> Confirm: D is linted, E is NOT linted (not necessary, since master was pushed earlier), push succeeds.'
+git push origin feature-branch
+echo ''
+
+echo '>> Commit. (Changes: Add F to feature branch.)'
+touch F.js
+git add F.js
+git commit -m 'Add F to feature branch.'
+echo ''
+
+echo '>> Checkout master.'
+git checkout master
+echo ''
+
+echo '>> Commit. (Changes: Add G.)'
+touch G.js
+git add G.js
+git commit -m 'Add G.'
+echo ''
+
+echo '>> (Unlike last time, do NOT push to master.)'
+echo ''
+
+echo '>> Checkout feature branch.'
+git checkout feature-branch
+echo ''
+
+echo '>> Merge master.'
+git merge master --no-edit
+echo ''
+
+echo '>> Push. (Changes: Add F to feature branch, merge G from master.)'
+echo '>> Confirm: F and G are linted, push succeeds.'
+git push origin feature-branch
+echo ''
+
+echo 'Test passed!'
