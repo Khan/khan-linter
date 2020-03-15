1ee98df809721b3607a082ab5a1a713984d2fa3b
Craig Silverstein <csilvers@khanacademy.org>
2020-03-14 22:10:04 -0700

    Add a linter for sdl (graphql "schema definition language") files.
    Summary:
    Mostly it's to make sure we document all our schema fields.
    
    We use the graphql-schema-linter tool to do our work.  We just have to
    parse the output and deal with it.
    
    I will fix up all existing errors in webapp before landing this.
    
    Issue: https://khanacademy.atlassian.net/browse/INFRA-4339
    
    Test Plan:
    I ran
       ka-lint ../../webapp/services/content/schema.graphql
    and it successfully ran, and emitted lines like:
       ../../webapp/services/content/schema.graphql:3:1: Eschema The object type `ArticleGql`s description should put the leading triple-quote on its own line. (types-have-descriptions)
    
    Reviewers: benkraft
    
    Subscribers: davidbraley, Kai, kevinb
    
    Differential Revision: https://phabricator.khanacademy.org/D61552

---
+++ graphql-schema-linter.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "rules": [
+        "deprecations-have-a-reason",
+        "enum-values-all-caps",
+        "fields-are-camel-cased",
+        "input-object-values-are-camel-cased",
+        "types-are-capitalized",
+    ]
+};
